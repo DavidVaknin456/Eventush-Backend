1f73153528b3ad961fbf7a67cd78d0681958fcb1
David <davidvaknin456@gmail.com>
2022-08-19 06:35:48 +0300

    get/add Events include tokenId Verification

--- app.js
+++ app.js
@@ -10,11 +10,48 @@ const app = express()
 
 app.use(express.json());
 
+// Basic Get
 app.get('/', (req, res) => {
     res.send("Heroku in your face")
     console.log("Basic get");
 });
 
+// Get User
+app.get('/getUser', (req, res, next) => {
+    console.log("getUser is active by user client")
+    const receivedToken = req.header('authorization');
+    if (receivedToken) {
+        const idToken = receivedToken.split(" ")[1];
+        // idToken comes from the client app
+        console.log(idToken)
+        getAuth().verifyIdToken(idToken)
+            .then((decodedToken) => {
+                Blog.findOne({uid: decodedToken.uid}, function (err, user) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("success");
+                        if (user === null) {
+                            console.log("the user is unregistered", user);
+                            res.send(false);
+                        }
+                        res.json(user);
+                        console.log(user);
+                    }
+                })}).catch((error) => {
+            console.log(error);
+            console.log("invalid token");
+            return res.sendStatus(403);
+        });
+    }
+    else {
+        res.sendStatus(401);
+    }
+
+});
+
+// Add User
 app.post('/post', async (req, res)  => {
     console.log("req post is active")
     const receivedToken = req.header('authorization');
@@ -51,40 +88,32 @@ app.post('/post', async (req, res)  => {
     }
 })
 
-app.get('/getUser', (req, res, next) => {
-    console.log("getUser is active by user client")
+
+// Get Events
+app.get("/events", async (req, res) => {
+    console.log("get events is active by client")
     const receivedToken = req.header('authorization');
     if (receivedToken) {
         const idToken = receivedToken.split(" ")[1];
         // idToken comes from the client app
         console.log(idToken)
         getAuth().verifyIdToken(idToken)
-            .then((decodedToken) => {
-                Blog.findOne({uid: decodedToken.uid}, function (err, user) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    else {
-                        console.log("success");
-                        if (user === null) {
-                            console.log("the user is unregistered", user);
-                            res.send(false);
-                        }
-                        res.json(user);
-                        console.log(user);
-                    }
-            })}).catch((error) => {
-                console.log(error);
-                console.log("invalid token");
-                return res.sendStatus(403);
-            });
+            .then(async (decodedToken) => {
+                const events = await Event.find();
+                console.log(events)
+                return res.status(200).json(events);
+            }).catch((error) => {
+            console.log(error);
+            console.log("invalid token");
+            return res.sendStatus(403);
+        });
     }
     else {
         res.sendStatus(401);
     }
-
 });
 
+// Add Event
 app.post('/add-Event', async (req, res)  => {
     console.log("addEvent post is active")
     const receivedToken = req.header('authorization');
@@ -96,7 +125,7 @@ app.post('/add-Event', async (req, res)  => {
             .then((decodedToken) => {
                 console.log(decodedToken.uid)
                 const event = new Event({
-                    orgId: decodedToken.uid,
+                    orgID: decodedToken.uid,
                     category: req.body.category,
                     date: req.body.date,
                     location: req.body.location,
@@ -122,12 +151,5 @@ app.post('/add-Event', async (req, res)  => {
         res.sendStatus(401);
     }
 })
-
-app.get("/events", async (req, res) => {
-    const events = await Event.find();
-
-    console.log(events[0])
-    return res.status(200).json(events);
-});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
