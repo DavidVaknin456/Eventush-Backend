838b788bf3d21df4187c5456ccd858fad8ec2c4b
David <davidvaknin456@gmail.com>
2022-09-20 20:15:58 +0300

    “-“

--- route/basic.js
+++ route/basic.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Basic Get
-router.get('/', (req, res) => {
-    res.send("Heroku in your face")
-    console.log("Basic get");
+router.get("/", (req, res) => {
+  res.send("Heroku in your face");
+  console.log("Basic get");
+});
+
+router.get("/video/progressive/cfid-111_stid-222_end.mp4", (req, res) => {
+  res.sendStatus(500);
+  console.log("res.sendStatus(500)");
 });
 
 module.exports = router;
