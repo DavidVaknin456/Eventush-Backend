75f70d2c606339219e1d1d924868f88ed0cac4c5
David <davidvaknin456@gmail.com>
2022-09-21 13:45:21 +0300

    “-“

--- route/basic.js
+++ route/basic.js
@@ -9,12 +9,12 @@ router.get("/", (req, res) => {
 
 router.get("/video/progressive/cfid-111_stid-222_end.mp4", (req, res) => {
   res.sendStatus(500);
-  console.log("res.sendStatus(500)");
+  console.log("res:     500");
 });
 
 router.get("/video/progressive/cfid-111_stid-25522_end.mp4", (req, res) => {
   res.sendStatus(200);
-  console.log("res.sendStatus(500)");
+  console.log("res:    200)");
 });
 
 module.exports = router;
